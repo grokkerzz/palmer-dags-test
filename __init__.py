4c8cb91594b3068d58532603c747e74a814b8672
Nguyen Huu Phuong <phuong.nguyenhuucse@gmail.com>
2020-02-25 11:10:06 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,28 +4,33 @@ import requests
 from util import logger, NOTIFICATION_EMAILS
 
 
-def on_success_callback(context):
+def on_success_callback(**context):
     logger.info('Success callback')
-    task_id = context['backend_task_id']
-    task_type = context['backend_task_type']
     logger.info(context)
+    task_instance = context['task_instance']
+    task_id = task_instance.xcom_pull(task_ids='running_update_status', key='backend_task_id')
+    task_type = task_instance.xcom_pull(task_ids='running_update_status' key='backend_task_type')
     data = { "status": "success" }
     url = "http://backend.dinovative.com/api/" + task_type + "/" + task_id
     r = requests.put(url, data)
 
 
-def on_failure_callback(context):
+def on_failure_callback(**context):
     logger.info('Fail callback')
-    task_id = context['backend_task_id']
-    task_type = context['backend_task_type']
     logger.info(context)
+    task_instance = context['task_instance']
+    task_id = task_instance.xcom_pull(task_ids='running_update_status', key='backend_task_id')
+    task_type = task_instance.xcom_pull(task_ids='running_update_status' key='backend_task_type')
     data = { "status": "failed" }
     url = "http://backend.dinovative.com/api/" + task_type + "/" + task_id
     r = requests.put(url, data)
 
 
-def on_running_callback(task_type, task_id):
+def on_running_callback(task_type, task_id, **context):
     data = { "status": "running" }
+    task_instance = context['task_instance']
+    task_instance.xcom_push(key='backend_task_id', value=task_id)
+    task_instance.xcom_push(key='backend_task_type', value=task_type)
     url = "http://backend.dinovative.com/api/" + task_type + "/" + task_id
     r = requests.put(url, data)
 
