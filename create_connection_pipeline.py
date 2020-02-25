1564ae047e9f4e084facc719e496c4fc63f3db55
Nguyen Huu Phuong <phuong.nguyenhuucse@gmail.com>
2020-02-25 12:06:29 +0700

    Update create_connection_pipeline.py

--- create_connection_pipeline.py
+++ create_connection_pipeline.py
@@ -12,7 +12,7 @@ from core.create_connection_pipeline.tasks import (
 create_connection_pipeline_every_minute_dag = DAG(
     'create_connection_pipeline_every_minute_dag',
     start_date=datetime.datetime(2019, 8, 21, 8, 20, 2, 84226),
-    schedule_interval='* * * * *',
+    schedule_interval='*/3 * * * *',
     default_args=default_system_args,
     catchup=False,
     template_searchpath=os.environ['AIRFLOW_HOME']
