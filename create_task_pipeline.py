d169da58c7cf4fd5b10cd3d4dceae6fe8da9a2ef
Nguyen Huu Phuong <phuong.nguyenhuucse@gmail.com>
2020-03-11 14:03:56 +0700

    Update create_task_pipeline.py

--- create_task_pipeline.py
+++ create_task_pipeline.py
@@ -10,10 +10,10 @@ from core.create_task_pipeline.tasks import (
     create_embulk_task, create_sql_task)
 
 
-create_task_pipeline_every_one_minute_dag = DAG(
-    'create_task_pipeline_every_one_minute_dag',
+create_task_pipeline_dag = DAG(
+    'create_task_pipeline_dag',
     start_date=datetime.datetime(2019, 8, 21, 8, 20, 2, 84226),
-    schedule_interval='* * * * *',
+    schedule_interval=None,
     default_args=default_system_args,
     catchup=False,
     template_searchpath=os.environ['AIRFLOW_HOME']
@@ -23,28 +23,28 @@ read_pending_embulk_task_operator = PythonOperator(
     task_id='read_pending_embulk_task',
     provide_context=True,
     python_callable=read_pending_embulk_task,
-    dag=create_task_pipeline_every_one_minute_dag
+    dag=create_task_pipeline_dag
 )
 
 read_pending_sql_task_operator = PythonOperator(
     task_id='read_pending_sql_task',
     provide_context=True,
     python_callable=read_pending_sql_task,
-    dag=create_task_pipeline_every_one_minute_dag
+    dag=create_task_pipeline_dag
 )
 
 create_embulk_task_operator = PythonOperator(
     task_id='create_embulk_task',
     provide_context=True,
     python_callable=create_embulk_task,
-    dag=create_task_pipeline_every_one_minute_dag
+    dag=create_task_pipeline_dag
 )
 
 create_sql_task_operator = PythonOperator(
     task_id='create_sql_task',
     provide_context=True,
     python_callable=create_sql_task,
-    dag=create_task_pipeline_every_one_minute_dag
+    dag=create_task_pipeline_dag
 )
 
 
