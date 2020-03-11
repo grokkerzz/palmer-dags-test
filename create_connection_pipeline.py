ec6cfa4c212598d95a77dbbfa2023e2e4df31e2e
Nguyen Huu Phuong <phuong.nguyenhuucse@gmail.com>
2020-03-11 14:04:16 +0700

    Update create_connection_pipeline.py

--- create_connection_pipeline.py
+++ create_connection_pipeline.py
@@ -9,10 +9,10 @@ from core.create_connection_pipeline.tasks import (
     read_pending_connection_task, update_connection_detail)
 
 
-create_connection_pipeline_every_minute_dag = DAG(
-    'create_connection_pipeline_every_minute_dag',
+create_connection_pipeline_dag = DAG(
+    'create_connection_pipeline_dag',
     start_date=datetime.datetime(2019, 8, 21, 8, 20, 2, 84226),
-    schedule_interval='* * * * *',
+    schedule_interval=None,
     default_args=default_system_args,
     catchup=False,
     template_searchpath=os.environ['AIRFLOW_HOME']
@@ -22,14 +22,14 @@ read_pending_connection_task_operator = PythonOperator(
     task_id='read_pending_connection_task',
     provide_context=True,
     python_callable=read_pending_connection_task,
-    dag=create_connection_pipeline_every_minute_dag
+    dag=create_connection_pipeline_dag
 )
 
 update_connection_detail_operator = PythonOperator(
     task_id='update_connection_detail',
     provide_context=True,
     python_callable=update_connection_detail,
-    dag=create_connection_pipeline_every_minute_dag
+    dag=create_connection_pipeline_dag
 )
 
 update_connection_detail_operator.set_upstream(
